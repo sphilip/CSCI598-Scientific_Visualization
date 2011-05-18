ceae6c9bed93b299501b79693a01cf76373fc388
Shanley Philip <sphilip@mymail.mines.edu>
2011-05-18 16:12:31 -0600

    able to read sequence of images and open each image

--- project1/raytrace.cpp
+++ project1/raytrace.cpp
@@ -5,15 +5,44 @@ CSCI 598
 **/
 
 #include <iostream>
+#include <fstream>
+#include <cstring>
+
 #include "image.h"
 
 using namespace std;
 
-int main(int argc, char** argv)
+void parseFile(char* filename)
 {
-  image img = image(256,256);
-  cout << "yellow" << endl;
-  return 0;
+
+  ifstream infile(filename);
+  if (!infile)
+  {
+    cout << "Couldn't open file " << filename << endl;
+
+  }
+
+  else
+  {
+  }
+
+  return;
 }
 
+int main(int argc, char** argv)
+{
+  if (argc < 2)
+  {
+    cout << "To execute, must include images\n\t" << argv[0] << "list of images\n";
+  }
 
+else
+{
+  for (int i=1; i<argc; i++)
+  {
+    cout << "Reading " << argv[i] << endl;
+    parseFile(argv[i]);
+  }
+}
+  return 0;
+}
