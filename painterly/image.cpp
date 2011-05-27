0a57f495957998375a4e62647eedc8a3eb165807
Shanley Philip <sphilip@mymail.mines.edu>
2011-05-26 22:28:22 -0600

    changed rounding from 256 to 255

--- painterly/image.cpp
+++ painterly/image.cpp
@@ -11,8 +11,6 @@ image::image ()
   ysize = 0;
 }
 
-
-
 image::image ( int m, int n ) : xsize(m), ysize(n)
 {
   rgb = new RGB[m*n];
@@ -65,9 +63,9 @@ void image::save_to_ppm_file (const char *filename )
   ofs << "P6" << endl;
   ofs << xsize << " " << ysize << endl << 255 << endl;
   for ( int i=0; i<xsize*ysize; i++ ) {
-    unsigned char r = clampnround(256*rgb[i].r);
-    unsigned char g = clampnround(256*rgb[i].g);
-    unsigned char b = clampnround(256*rgb[i].b);
+    unsigned char r = clampnround(255*rgb[i].r);
+    unsigned char g = clampnround(255*rgb[i].g);
+    unsigned char b = clampnround(255*rgb[i].b);
     ofs.write((char*)&r,sizeof(char));
     ofs.write((char*)&g,sizeof(char));
     ofs.write((char*)&b,sizeof(char));
