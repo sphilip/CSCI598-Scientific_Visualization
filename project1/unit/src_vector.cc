733562f9e6ba86130d2c28f11fb55942c93ff822
Shanley Philip <sphilip.home@gmail.com>
2013-05-26 08:58:34 -0600

    [UNIT TEST] [src_vector.cc] Changed asserts to expects so test case can finish & then complain Added subtraction, negate, scalar multiply, magnitude, normalization tests   I disabled a scalar multiply test (ie. 2*vector) b/c it doesn't work and     I don't know how to fix it

--- project1/unit/src_vector.cc
+++ project1/unit/src_vector.cc
@@ -7,18 +7,18 @@ TEST(VectorTest, DefaultConstructor) {
     
     Vector test;
     
-    ASSERT_EQ(0.0, test.x);
-    ASSERT_EQ(0.0, test.y);
-    ASSERT_EQ(0.0, test.z);
+    EXPECT_EQ(0.0, test.x);
+    EXPECT_EQ(0.0, test.y);
+    EXPECT_EQ(0.0, test.z);
 }
 
 TEST(VectorTest, UserDefinedConstructor) {
     
     Vector test(2.0, -55, 3.14563);
     
-    ASSERT_EQ(2.0,     test.x);
-    ASSERT_EQ(-55.0,   test.y);
-    ASSERT_EQ(3.14563, test.z);
+    EXPECT_EQ(2.0,     test.x);
+    EXPECT_EQ(-55.0,   test.y);
+    EXPECT_EQ(3.14563, test.z);
 }
 
 TEST(VectorTest, OperatorAdd) {
@@ -27,15 +27,78 @@ TEST(VectorTest, OperatorAdd) {
     Vector b(1.0, -2.0, 3.0);
     Vector c = a+b;
 
-    ASSERT_EQ(1.0,  c.x);
-    ASSERT_EQ(-2.0, c.y);
-    ASSERT_EQ(3.0,  c.z);
+    EXPECT_EQ(1.0,  c.x);
+    EXPECT_EQ(-2.0, c.y);
+    EXPECT_EQ(3.0,  c.z);
 }
 
-TEST(VectorTest, MagnitudePostiveValues) {
+TEST(VectorTest, OperatorSubtract) {
+
+    Vector a;
+    Vector b(0.0, -2.0, 3.0);
+    Vector c = a-b;
+
+    EXPECT_EQ(0.0,  c.x);
+    EXPECT_EQ(2.0, c.y);
+    EXPECT_EQ(-3.0,  c.z);
+}
+
+TEST(VectorTest, NegateVector) {
+
+    Vector b(0.0, -2.0, 3.0);
+    Vector c = -b;
+
+    EXPECT_EQ(0.0,  c.x);
+    EXPECT_EQ(2.0, c.y);
+    EXPECT_EQ(-3.0,  c.z);
+}
+
+TEST(VectorTest, ScaleVectorPrefix) {
+
+    Vector b(0.0, -2.0, 3.0);
+    Vector c = b*2.0;
+
+    EXPECT_EQ(0.0,  c.x);
+    EXPECT_EQ(-4.0, c.y);
+    EXPECT_EQ(6.0,  c.z);
+}
+
+TEST(VectorTest, DISABLED_ScaleVectorPostfix) {
+
+    /*
+    Vector b(0.0, -2.0, 3.0);
+    Vector c = 2.0*b;
+
+    EXPECT_EQ(0.0,  c.x);
+    EXPECT_EQ(-4.0, c.y);
+    EXPECT_EQ(6.0,  c.z);
+    */
+}
+TEST(VectorTest, MagnitudePositiveValues) {
 
     Vector test(1.0, 2.0, 3.0);
+    double actual = test.magnitude();
+    double expected = sqrt(pow(test.x,2) + pow(test.y,2) + pow(test.z,2));
 
+    ASSERT_EQ(expected, actual);
+}
+
+TEST(VectorTest, MagnitudeNegativeValues) {
+
+    Vector test(-1.0, -2.0, -3.0);
+    double actual = test.magnitude();
+    double expected = sqrt(pow(test.x,2) + pow(test.y,2) + pow(test.z,2));
+
+    ASSERT_EQ(expected, actual);
+}
+
+TEST(VectorTest, MagnitudeMixedValues) {
+
+    Vector test(1.0, 0, -3.0);
+    double actual = test.magnitude();
+    double expected = sqrt(pow(test.x,2) + pow(test.z,2));
+
+    ASSERT_EQ(expected, actual);
 }
 
 TEST(VectorTest, NormalizePositiveValues) {
@@ -44,7 +107,30 @@ TEST(VectorTest, NormalizePositiveValues) {
     Vector normal = test.normalize();
 
     double magn = test.magnitude();
-    ASSERT_EQ(test.x/magn, normal.x);
-    ASSERT_EQ(test.y/magn, normal.y);
-    ASSERT_EQ(test.z/magn, normal.z);
+    EXPECT_EQ(test.x/magn, normal.x);
+    EXPECT_EQ(test.y/magn, normal.y);
+    EXPECT_EQ(test.z/magn, normal.z);
+}
+
+TEST(VectorTest, NormalizeNegativeValues) {
+    
+    Vector test(-2.0, -2.0, -2.0);
+    Vector normal = test.normalize();
+
+    double magn = test.magnitude();
+    EXPECT_EQ(test.x/magn, normal.x);
+    EXPECT_EQ(test.y/magn, normal.y);
+    EXPECT_EQ(test.z/magn, normal.z);
+}
+
+
+TEST(VectorTest, NormalizeMixedValues) {
+    
+    Vector test(-2.0, 0, 2.0);
+    Vector normal = test.normalize();
+
+    double magn = test.magnitude();
+    EXPECT_EQ(test.x/magn, normal.x);
+    EXPECT_EQ(test.y/magn, normal.y);
+    EXPECT_EQ(test.z/magn, normal.z);
 }
