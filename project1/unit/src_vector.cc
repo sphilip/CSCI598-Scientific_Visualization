657316e06bac0c2562ea01b813efa4badc5a1e0a
Shanley Philip <sphilip.home@gmail.com>
2013-05-25 21:55:15 -0600

    [UNIT TEST] [src_vector.cc] Removed sample test Added tests for default & user-defined constructor Started testing magnitude & normalize function   Still need to test using negative values & mixed (ie. negative & positive)

--- project1/unit/src_vector.cc
+++ project1/unit/src_vector.cc
@@ -1,6 +1,50 @@
-#include "gtest/gtest.h"
+#include <cmath>
+#include <gtest/gtest.h>
+
 #include "../src/Vector.h"
 
-TEST(TestingTest, DoesItWork) {
-    ASSERT_TRUE(true);
+TEST(VectorTest, DefaultConstructor) {
+    
+    Vector test;
+    
+    ASSERT_EQ(0.0, test.x);
+    ASSERT_EQ(0.0, test.y);
+    ASSERT_EQ(0.0, test.z);
+}
+
+TEST(VectorTest, UserDefinedConstructor) {
+    
+    Vector test(2.0, -55, 3.14563);
+    
+    ASSERT_EQ(2.0,     test.x);
+    ASSERT_EQ(-55.0,   test.y);
+    ASSERT_EQ(3.14563, test.z);
+}
+
+TEST(VectorTest, OperatorAdd) {
+
+    Vector a;
+    Vector b(1.0, -2.0, 3.0);
+    Vector c = a+b;
+
+    ASSERT_EQ(1.0,  c.x);
+    ASSERT_EQ(-2.0, c.y);
+    ASSERT_EQ(3.0,  c.z);
+}
+
+TEST(VectorTest, MagnitudePostiveValues) {
+
+    Vector test(1.0, 2.0, 3.0);
+
+}
+
+TEST(VectorTest, NormalizePositiveValues) {
+    
+    Vector test(2.0, 2.0, 2.0);
+    Vector normal = test.normalize();
+
+    double magn = test.magnitude();
+    ASSERT_EQ(test.x/magn, normal.x);
+    ASSERT_EQ(test.y/magn, normal.y);
+    ASSERT_EQ(test.z/magn, normal.z);
 }
